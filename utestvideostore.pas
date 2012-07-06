3db58a7c11839b05f7762612ccbe868b2a9ff576
Martin Gondermann <magicmonty@pagansoft.de>
2012-07-06 11:41:30 +0200

    Made the base work in branch freepascal

--- utestvideostore.pas
+++ utestvideostore.pas
@@ -3,7 +3,9 @@ unit uTestVideoStore;
 interface
 
 uses
-  TestFramework, Classes, uMovie, uRental, uCustomer;
+  Classes,
+  fpcunit, testregistry,
+  uMovie, uRental, uCustomer;
 
 type
   TestVideoStore = class(TTestCase)
@@ -35,20 +37,20 @@ end;
 procedure TestVideoStore.TestSingleNewReleaseStatement;
 begin
   FCustomer.AddRental(TRental.Create(TMovie.Create('The Cell', mtNewRelease), 3));
-  CheckEqualsString('Rental Record for Fred' + #13#10 + #9 + 'The Cell' + #9 + '9,00' + #13#10 + 'You owed 9,00' + #13#10 + 'You earned 2 frequent renter points' + #13#10, FCustomer.Statement);
+  CheckEquals('Rental Record for Fred' + #13#10 + #9 + 'The Cell' + #9 + '9.00' + #13#10 + 'You owed 9.00' + #13#10 + 'You earned 2 frequent renter points' + #13#10, FCustomer.Statement);
 end;
 
 procedure TestVideoStore.TestDualNewReleaseStatement;
 begin
   FCustomer.AddRental(TRental.Create(TMovie.Create('The Cell', mtNewRelease), 3));
   FCustomer.AddRental(TRental.Create(TMovie.Create('The Tigger Movie', mtNewRelease), 3));
-  CheckEqualsString('Rental Record for Fred' + #13#10 + #9 + 'The Cell' + #9 + '9,00' + #13#10 + #9 + 'The Tigger Movie' + #9 + '9,00' + #13#10 + 'You owed 18,00' + #13#10 + 'You earned 4 frequent renter points' + #13#10, FCustomer.Statement);
+  CheckEquals('Rental Record for Fred' + #13#10 + #9 + 'The Cell' + #9 + '9.00' + #13#10 + #9 + 'The Tigger Movie' + #9 + '9.00' + #13#10 + 'You owed 18.00' + #13#10 + 'You earned 4 frequent renter points' + #13#10, FCustomer.Statement);
 end;
 
 procedure TestVideoStore.TestSingleChildrenStatement;
 begin
   FCustomer.AddRental(TRental.Create(TMovie.Create('The Tigger Movie', mtChildrens), 3));
-  CheckEqualsString('Rental Record for Fred' + #13#10 + #9 + 'The Tigger Movie' + #9 + '1,50' + #13#10 + 'You owed 1,50' + #13#10 + 'You earned 1 frequent renter points' + #13#10, FCustomer.Statement);
+  CheckEquals('Rental Record for Fred' + #13#10 + #9 + 'The Tigger Movie' + #9 + '1.50' + #13#10 + 'You owed 1.50' + #13#10 + 'You earned 1 frequent renter points' + #13#10, FCustomer.Statement);
 end;
 
 procedure TestVideoStore.TestMultipleRegularStatement;
@@ -57,10 +59,10 @@ begin
   FCustomer.AddRental(TRental.Create(TMovie.Create('8 1/2', mtRegular), 2));
   Fcustomer.addRental(TRental.Create(TMovie.Create('Eraserhead', mtRegular), 3));
 
-  CheckEqualsString('Rental Record for Fred' + #13#10 + #9 + 'Plan 9 from Outer Space' + #9 + '2,00' + #13#10 + #9 + '8 1/2' + #9 + '2,00' + #13#10 + #9 + 'Eraserhead' + #9 + '3,50' + #13#10 + 'You owed 7,50' + #13#10 + 'You earned 3 frequent renter points' + #13#10, FCustomer.Statement);
+  CheckEquals('Rental Record for Fred' + #13#10 + #9 + 'Plan 9 from Outer Space' + #9 + '2.00' + #13#10 + #9 + '8 1/2' + #9 + '2.00' + #13#10 + #9 + 'Eraserhead' + #9 + '3.50' + #13#10 + 'You owed 7.50' + #13#10 + 'You earned 3 frequent renter points' + #13#10, FCustomer.Statement);
 end;
 
 initialization
-  RegisterTest(TestVideoStore.Suite);
+  RegisterTest(TestVideoStore);
 end.
-
+
